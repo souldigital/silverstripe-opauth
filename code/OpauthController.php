2d55622de46cd8081ba2ed96453ab10e008df584
Senorgeno <genedower@gmail.com>
2014-08-27 12:10:53 +1200

    Pass config settings to find create member method and added on after hook

--- code/OpauthController.php
+++ code/OpauthController.php
@@ -116,10 +116,12 @@ class OpauthController extends ContentController {
 			return $this->handleOpauthException($e);
 		}
 
-		$identity = OpauthIdentity::factory($response);
-
-		$member = $identity->findOrCreateMember();
-
+		$identity = OpauthIdentity::factory($response);		
+		
+		$settings = Config::inst()->get('OpauthIdentity', 'user_settings');
+		
+		$member = $identity->findOrCreateMember($settings);
+		
 		// If the member exists, associate it with the identity and log in
 		if($member->isInDB() && $member->validate()->valid()) {
 			if(!$identity->exists()) {
@@ -152,6 +154,7 @@ class OpauthController extends ContentController {
 			else {
 				$member->extend('onBeforeOpauthRegister');
 				$member->write();
+				$member->extend('onAfterOpauthRegister');
 				$identity->MemberID = $member->ID;
 				$identity->write();
 			}
